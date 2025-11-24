e7afa1ab1c463ecd90b592ddbd1d70fea3914477
JulianG1525 <Gamet.Julian@student.greenriver.edu>
2025-11-24 15:15:11 -0800

    Utilized javascript in code

---
+++ buttons.js
@@ -0,0 +1,19 @@
+/* Block comment */
+// Single line comment
+// Showing that the JS is being loaded
+console.log("Hello");
+
+function logPress() {
+    console.log("Good job!");
+    console.log("You clicked a button!");
+}
+function showAlert() {
+    alert("Hello user!");
+}
+
+//Make new function toogledark
+// log "toggleDark called"
+function toggleDark() {
+    console.log("toggleDark called");
+    document.body.classList.toggle("dark-mode");
+}
